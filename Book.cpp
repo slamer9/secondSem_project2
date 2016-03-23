34dda583793a116626e60c49032120b20b4a98d2
slamer9 <duncansreeves@gmail.com>
2016-03-22 23:26:18 -0600

    Home

--- Book.cpp
+++ Book.cpp
@@ -1,5 +1,8 @@
 #include "stdafx.h"
 #include "Book.h"
+#include <iostream>
+#include <fstream>
+#include <ostream>
 
 using namespace std;
 
@@ -29,5 +32,18 @@ int Book::getNumOfPages()
 double Book::getPrice()
 {
 	return price;
+}
+
+void Book::writeData(ofstream& myOfStream)
+{
+	if (myOfStream.fail())
+	{
+		cout << "There was an error in creating the file" << endl;
+	}
+	else
+	{
+		myOfStream << myAuthor.getName() << endl << myAuthor.getAdress() << endl <<
+			title << endl << numOfPages << endl << price << endl << endl;
+	}
 }
 
